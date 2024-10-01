de1b4dd10e46da50eeebf5cc77447bc7c6e56397
EMSMoreno <emsmoreno97@gmail.pt>
2024-10-01 21:08:34 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //var navPage = new NavigationPage(new MinhaPagina());
+
+            //navPage.BarBackground = Colors.Yellow;
+            //navPage.BarTextColor = Colors.Blue;
+
+            MainPage = new MainPage();
+        }
+    }
+}
