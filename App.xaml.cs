ca04b8453821d86f0074176c34434914efd87f03
EMSMoreno <emsmoreno97@gmail.pt>
2024-10-03 10:52:02 +0100

    MauiEditControls

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage (new MauiSetValueControls());
+            MainPage = new NavigationPage (new MauiEditControls());
         }
     }
 }
